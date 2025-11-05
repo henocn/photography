aedf4ffde64a2713c562bcfb8d96ddd279933fb6
Henoc N'GASAMA <ngasamah@gmail.com>
2025-11-05 10:46:19 +0000

    fix: One2many writting fro equipement ids in project model class

--- models/photography_project.py
+++ models/photography_project.py
@@ -14,7 +14,7 @@ class PhotographyProject(models.Model):
 
     client_id = fields.Many2one('res.partner', string="Client", required=True)
     furniture_ids = fields.One2many('photography.furniture', 'project_id', string="Meubles utilisés")
-    equipement_ids = fields.One2manay('photography.equipement', 'project_id', string="Équipements utilisés")
+    equipment_ids = fields.One2many('photography.equipment', 'project_id', string="Équipements utilisés")
     media_ids = fields.One2many('photography.media', 'project_id', string="Médias associés")
     layout_plan = fields.Selection(layout_plan_selection, string="Plan de disposition", default='annotated_photo', required=True)
     shooting_date = fields.Datetime(string="Date de la séance photo")
