22246fcabd65690811f55bc27d2e0934f53a1f85
Henoc N'GASAMA <ngasamah@gmail.com>
2025-11-05 10:40:49 +0000

    add equipement model class

---
+++ models/photography_equipment.py
@@ -0,0 +1,22 @@
+from odoo import fields, models
+
+
+class PhotographyEquipment(models.Model):
+    _name = "photography.equipment"
+    _description = "Équipement photographique"
+
+    type_selection = [
+        ('camera', 'Appareil photo'),
+        ('lens', 'Objectif'),
+        ('tripod', 'Trépied'),
+        ('lighting', 'Éclairage'),
+        ('reflector', 'Réflecteur'),
+        ('microphone', 'Microphone'),
+        ('microcravate', 'Micro-cravate'),
+        ('other', 'Autre'),
+    ]
+
+    project_id = fields.Many2one('photography.project', string="Projet associé", required=True)
+    equipment_type = fields.Selection(type_selection, string="Nom de l'équipement", required=True)
+    quantity = fields.Integer(string="Quantité", default=1)
+    description = fields.Text(string="Description de l'équipement")
