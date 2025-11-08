44e68184ca74b82f9e658c5beb93d03151d2d98f
Henoc N'GASAMA <ngasamah@gmail.com>
2025-11-08 10:06:51 +0000

    adapt equipment model to external data

--- models/photography_equipment.py
+++ models/photography_equipment.py
@@ -6,17 +6,20 @@ class PhotographyEquipment(models.Model):
     _description = "Équipement photographique"
 
     type_selection = [
-        ('camera', 'Appareil photo'),
-        ('lens', 'Objectif'),
-        ('tripod', 'Trépied'),
-        ('lighting', 'Éclairage'),
-        ('reflector', 'Réflecteur'),
-        ('microphone', 'Microphone'),
-        ('microcravate', 'Micro-cravate'),
-        ('other', 'Autre'),
+        ("audio", "AUDIO"),
+        ("video", "VIDEO"),
+        ("audio_video", "AUDIO&VIDEO"),
+        ("lumiere", "LUMIERE"),
+    ]
+
+    using_state_selection = [
+        ("new", "NEUF"),
+        ("on_using", "EN SERVICE"),
     ]
 
     image = fields.Image(string="Photo de l'équipement", required=True)
+    name = fields.Char(string="Nom de l'équipment", required=True)
     equipment_type = fields.Selection(type_selection, string="Nom de l'équipement", required=True)
+    equipment_state = fields.Selection(using_state_selection, string="Etat d'utilisation", required=True)
     quantity = fields.Integer(string="Quantité disponible", default=1)
     description = fields.Text(string="Description de l'équipement")
