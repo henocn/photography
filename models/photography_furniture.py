0ce4d465358cbe5db1d0f0b39fd1ca14a8c84634
Henoc N'GASAMA <ngasamah@gmail.com>
2025-11-15 16:05:02 +0000

    add furniture state and it description modufying

--- models/photography_furniture.py
+++ models/photography_furniture.py
@@ -12,9 +12,16 @@ class PhotographyFurniture(models.Model):
         ('other', 'Autre'),
     ]
 
+    using_state_selection = [
+        ("new", "NEUF"),
+        ("on_using", "EN SERVICE"),
+        ("unknown", "NON SPECIFIE"),
+    ]
+
     image = fields.Image(string="Photo du meuble", required=True)
     name = fields.Char(string="Nom du meuble", required=True)
     furniture_type = fields.Selection(type_selection, string="Type de meuble", required=True)
+    furniture_state = fields.Selection(using_state_selection, string="Etat d'utilisation", required=True)
     location = fields.Char(string="Emplacement")
     quantity = fields.Integer(string="Quantité", default=1)
     description = fields.Text(string="Description")
