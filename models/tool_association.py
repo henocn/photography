44cd72399f392d2738e73e119385cd1e49c996f4
Henoc N'GASAMA <ngasamah@gmail.com>
2025-11-20 11:01:26 +0000

    add image to tool selection

--- models/tool_association.py
+++ models/tool_association.py
@@ -16,6 +16,7 @@ class ToolAssociation(models.Model):
     employee_id = fields.Many2one('rh.employee', string='Employé', required=True)
     tool_id = fields.Many2one('rh.tool', string='Outil', required=True)
     assigned_date = fields.Date(string='Date de remise', required=True)
+    image = fields.Image(related="tool_id.image", string="Image", store=False)
     tool_state = fields.Selection(tool_state_selection, string='Outil à la remise', default='good')
     quantity = fields.Integer(string='Quantité', default=1)
     additionnal_notes = fields.Text(string='Notes evantuelles')
